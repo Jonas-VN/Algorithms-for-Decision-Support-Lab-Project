4f3a434f87a667f70c359d620fdd4daeafe9767c
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2023-10-02 12:12:35 +0200

    location setters

--- src/Location.java
+++ src/Location.java
@@ -6,4 +6,12 @@ public class Location {
         this.x = x;
         this.y = y;
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }

6cb129f452346cd0393533842fd8b1e75ef825f0
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2023-12-08 16:28:58 +0100

    Add check if multiple vehicles are using a stack at the same time

--- src/Utils/Location.java
+++ src/Utils/Location.java
@@ -1,5 +1,7 @@
 package Utils;
 
+import java.util.Objects;
+
 public class Location {
     private int x;
     private int y;
@@ -29,6 +31,19 @@ public class Location {
                 "x=" + x +
                 ", y=" + y +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return x == location.x && y == location.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
 
--- src/Validator.java
+++ src/Validator.java
@@ -1,17 +1,40 @@
 import Utils.Location;
-import Warehouse.Warehouse;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class Validator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String outputFile = Main.problem;
-        final int loadDuration = 5;
+        HashMap<Location, Integer> lastAccessed = new HashMap<>();
+        int warnings = 0;
+        int errors = 0;
 
         String csvFile = "src/Output/src/output" + outputFile + ".txt";
         System.out.println(csvFile);
+
+        String jsonContent = new String(Files.readAllBytes(new File("src/Input/src/I" + outputFile + ".json").toPath()));
+        JSONObject jsonData = new JSONObject(jsonContent);
+        final int loadDuration = jsonData.getInt("loadingduration");
+        final int speed = jsonData.getInt("vehiclespeed");
+
+        ArrayList<Location> bufferPointLocations = new ArrayList<>();
+        JSONArray bufferPoints = jsonData.getJSONArray("bufferpoints");
+        for (int idx = 0; idx < bufferPoints.length(); idx++) {
+            JSONObject bufferPointData = bufferPoints.getJSONObject(idx);
+            int bufferPointX = bufferPointData.getInt("x");
+            int bufferPointY = bufferPointData.getInt("y");
+            bufferPointLocations.add(new Location(bufferPointX, bufferPointY));
+        }
+
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             int lineCount = 0;
             String line;
@@ -19,7 +42,7 @@ public class Validator {
             Location destinationLocation;
             int startTime;
             int endTime;
-            int distance;
+            int timeNeeded;
 
             while ((line = br.readLine()) != null) {
                 lineCount++;
@@ -29,16 +52,31 @@ public class Validator {
                 startTime = Integer.parseInt(values[3]);
                 endTime = Integer.parseInt(values[6]);
 
-                distance = startLocation.manhattanDistance(destinationLocation);
-                if (startTime + distance + loadDuration < endTime)
-                    System.out.println(lineCount + ": LOST TIME: " + line);
-                else if (startTime + distance + loadDuration > endTime)
-                    System.out.println(lineCount + ": UNFEASIBLE: " + line);
+                if (!lastAccessed.containsKey(destinationLocation)) lastAccessed.put(destinationLocation, 0);
+
+                timeNeeded = startLocation.manhattanDistance(destinationLocation) / speed;
+                if (startTime + timeNeeded + loadDuration < endTime) {
+                    System.out.println(lineCount + " WARNING: LOST TIME " + line);
+                    warnings++;
+                }
+                if (startTime + timeNeeded + loadDuration > endTime) {
+                    System.out.println(lineCount + "ERROR: UNFEASIBLE TIME " + line);
+                    errors++;
+                }
+
+                // If a vehicle freed a stack at time t then next vehicle can free it as soon as t + loadDuration
+                if (lastAccessed.get(destinationLocation) + loadDuration > endTime && !bufferPointLocations.contains(destinationLocation)) {
+                    System.out.println(lineCount + " ERROR: UNFEASIBLE LOADING" + line);
+                    errors++;
+                }
+                lastAccessed.put(destinationLocation, endTime);
+
+                // No need to validate if the storages would be full or empty since our simulation would have thrown an error
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            System.out.println("Finished validating");
+            System.out.println("Finished validating with " + warnings + " warnings and " + errors + " errors.");
         }
     }
 }
